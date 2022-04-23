7d7de369c97dda46dc0bfb163b30984b713c5d8a
Alexander Melo <alexandermelo@MacBook-Pro-de-Alexander.local>
2022-04-23 13:29:21 -0300

    rename replica instances

--- mongo-setup/mongo-setup.js
+++ mongo-setup/mongo-setup.js
@@ -3,17 +3,17 @@ rsconf = {
     members: [
         {
             "_id": 0,
-            "host": "mongodb1:27017",
+            "host": "mongodb_replica_mongodb1:27017",
             "priority": 3
         },
         {
             "_id": 1,
-            "host": "mongodb2:27017",
+            "host": "mongodb_replica_mongodb2:27017",
             "priority": 2
         },
         {
             "_id": 2,
-            "host": "mongodb3:27017",
+            "host": "mongodb_replica_mongodb3:27017",
             "priority": 1
         }
     ]
